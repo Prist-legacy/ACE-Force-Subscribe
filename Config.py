02f8db20b0c87a7e184b20bf9e5a76acecebb488
PRIST LEGACY <yiigavicent@gmail.com>
2023-03-28 21:15:53 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,13 +2,13 @@ import os
 
 class Config():
   # Bot Token (Use @BotFather)
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6012311237:AAHfLqvDqDS3N9OTwuJYMYXrTJrhwC1KgO4")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "5853392984:AAE6C7DhVtxGAINDFo5PDjMOBSLqhRKm61U")
 
   # Bot Updates Channel Username (without @)
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "ufmmatches")
 
   # PostgresSQL DB URL (Use ElephantSQL)
-  DATABASE_URL = os.environ.get("DATABASE_URL", "Pb1LF1h0ol1RlzNY6WuAVp_-p7eKbN9z@mouse.db.elephantsql.com")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://kstmkuef:Pb1LF1h0ol1RlzNY6WuAVp_-p7eKbN9z@mouse.db.elephantsql.com/kstmkuef")
 
   # API & HASH (Use my.telegram.org)
   APP_ID = os.environ.get("APP_ID", 20818634)
